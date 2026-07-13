dc3da9a4143309f4e63e45c01318ef30921a5f2c
Thais Holanda <thaismdh@gmail.com>
2026-07-12 22:55:18 -0300

    Replace icon-512.png with icon.svg in ASSETS

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE='horizonte-v7';
-const ASSETS=['./','index.html','styles.css?v=7','app.js?v=7','manifest.webmanifest','icon-192.png','icon-512.png'];
+const ASSETS=['./','index.html','styles.css?v=7','app.js?v=7','manifest.webmanifest','icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
 self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).then(res=>{const copy=res.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return res}).catch(()=>caches.match(e.request).then(r=>r||caches.match('index.html')))));
