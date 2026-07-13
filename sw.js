5050fcaf8e456a2e833e4cfde94e069ccd22bdff
Thais Holanda <thaismdh@gmail.com>
2026-07-12 21:50:25 -0300

    Manter aplicativo sempre atualizado

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='plano-estudos-v2';
+const CACHE='plano-estudos-v3';
 const ASSETS=['./','index.html','styles.css','app.js','manifest.webmanifest','icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{const copy=res.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return res}).catch(()=>caches.match('index.html')))));
+self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).then(res=>{const copy=res.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return res}).catch(()=>caches.match(e.request).then(r=>r||caches.match('index.html')))));
