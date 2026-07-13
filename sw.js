cf735612bcaa7b32d8c669c67c442a56ecc57677
Thais Holanda <thaismdh@gmail.com>
2026-07-13 17:07:21 -0300

    Update cache version and asset file versions

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='horizonte-v9';
-const ASSETS=['./','index.html','styles.css?v=9','app.js?v=9','manifest.webmanifest','icon.svg'];
+const CACHE='horizonte-v10';
+const ASSETS=['./','index.html','styles.css?v=10','app.js?v=10','manifest.webmanifest','icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
 self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).then(res=>{const copy=res.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return res}).catch(()=>caches.match(e.request).then(r=>r||caches.match('index.html')))));
