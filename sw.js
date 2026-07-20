3c18ace2d8aef44e34903ae8e1e328dc1f6a48c4
Thais Holanda <thaismdh@gmail.com>
2026-07-20 19:17:55 -0300

    Atualizar cache para limite diário

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='horizonte-v16';
-const ASSETS=['./','index.html','styles.css?v=16','app.js?v=16','manifest.webmanifest','icon.svg'];
+const CACHE='horizonte-v17';
+const ASSETS=['./','index.html','styles.css?v=17','app.js?v=17','manifest.webmanifest','icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
 self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).then(res=>{const copy=res.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return res}).catch(()=>caches.match(e.request).then(r=>r||caches.match('index.html')))));
